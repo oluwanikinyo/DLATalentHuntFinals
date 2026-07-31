9f17f6154dabd816d53ad9186053aa0716468a04
rukkiechovwe <rukkiechowe@gmail.com>
2026-07-31 15:33:01 +0100

    fix: update API URL to the correct endpoint

--- src/environments.ts
+++ src/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://staging-dla-teens-registration-backend-production.up.railway.app/api/v1',
+  apiUrl: 'https://teens-api.dlaonline.org/',
 };
